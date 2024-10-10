9d4238acfb6a5047d9e39422a9c0d70e6e1a1db6
양정화 <siltarre@gmail.com>
2024-10-10 18:54:08 +0900

    link 이동 부분 연결

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./components/App";
 import HomePage from "./pages/HomePage";
+import CourseListPage from "./pages/CourseListPage";
+import CoursePage from "./pages/CoursePage";
+import WishlistPage from "./pages/WishlistPage";
 
 function Main() {
   return (
     <BrowserRouter>
       <App>
-        <HomePage />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='courses' element={<CourseListPage />} />
+          <Route
+            path='courses/react-frontend-development'
+            element={<CoursePage />}
+          />
+          <Route path='wishlist' element={<WishlistPage />} />
+        </Routes>
       </App>
     </BrowserRouter>
   );
--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -1,6 +1,7 @@
-import { useCallback, useEffect, useState } from 'react';
-import personIcon from '../assets/person.png';
-import styles from './UserMenu.module.css';
+import { useCallback, useEffect, useState } from "react";
+import personIcon from "../assets/person.png";
+import styles from "./UserMenu.module.css";
+import { Link } from "react-router-dom";
 
 function UserMenu() {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,21 +15,24 @@ function UserMenu() {
     if (!isOpen) return;
 
     const handleClickOutside = () => setIsOpen(false);
-    window.addEventListener('click', handleClickOutside);
+    window.addEventListener("click", handleClickOutside);
 
     return () => {
-      window.removeEventListener('click', handleClickOutside);
+      window.removeEventListener("click", handleClickOutside);
     };
   }, [isOpen]);
 
   return (
     <div className={styles.userMenu}>
       <button className={styles.iconButton} onClick={handleButtonClick}>
-        <img src={personIcon} alt="유저 메뉴" />
+        <img src={personIcon} alt='유저 메뉴' />
       </button>
       {isOpen && (
         <ul className={styles.popup}>
-          <li>위시리스트</li>
+          <li>
+            <Link to='/wishlist'>위시리스트</Link>
+          </li>
+
           <li className={styles.disabled}>회원가입</li>
           <li className={styles.disabled}>로그인</li>
         </ul>
