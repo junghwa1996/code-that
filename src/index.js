82f5cd2dab55125696476e54d2328aa229748679
양정화 <siltarre@gmail.com>
2024-10-10 18:25:08 +0900

    라우터 설치

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import ReactDOM from 'react-dom';
-import Main from './Main';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Main from "./Main";
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main />);
