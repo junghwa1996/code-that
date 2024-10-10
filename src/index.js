f2520acce4100aee6cac487901b3d025a49fd609
양정화 <siltarre@gmail.com>
2024-10-10 18:04:47 +0900

    강의 세팅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from 'react-dom';
+import Main from './Main';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(<Main />, document.getElementById('root'));
